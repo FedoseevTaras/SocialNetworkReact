0d2c529510eca137e2e757b56e6995415c8538f8
Taras <fedoseevt@gmail.com>
2021-11-24 16:16:17 +0200

    1.14 components folder create

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://image.shutterstock.com/image-vector/dots-letter-c-logo-design-260nw-551769190.jpg"
+        alt=""
+      />
+    </header>
+  );
+};
+export default Header;
